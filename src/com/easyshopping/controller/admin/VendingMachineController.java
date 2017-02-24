48a778bc3ad194be32f94334e7fd9e78826def81
zhangzhaoguo <zzhaog@126.com>
2017-02-24 23:14:22 +0800

    售货机管理完善图片相关功能（增加、编辑、显示）

--- src/com/easyshopping/controller/admin/VendingMachineController.java
+++ src/com/easyshopping/controller/admin/VendingMachineController.java
@@ -1,12 +1,20 @@
 package com.easyshopping.controller.admin;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.easyshopping.Message;
@@ -40,10 +48,24 @@ public class VendingMachineController extends BaseController {
 	 * 保存
 	 */
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String save(vendor bean, RedirectAttributes redirectAttributes) {
+	public String save(vendor bean, RedirectAttributes redirectAttributes, @RequestParam("imageFile") MultipartFile file, HttpSession session) {
 		if (!isValid(bean, Save.class)) {
 			return ERROR_VIEW;
 		}
+		String relativePath = "upload\\image\\vendingMachine\\" + date2FileName("yyyyMMdd_HHmmss", ".jpg");
+		String path=session.getServletContext().getRealPath("/") + relativePath;
+		File newFile=new File(path);
+		//通过CommonsMultipartFile的方法直接写文件
+        try {
+			file.transferTo(newFile);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			return ERROR_VIEW;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ERROR_VIEW;
+		}
+        bean.setImage(relativePath);
 		vendingMachineService.save(bean);
 		addFlashMessage(redirectAttributes, SUCCESS_MESSAGE);
 		return "redirect:list.jhtml";
@@ -62,9 +84,25 @@ public class VendingMachineController extends BaseController {
 	 * 更新
 	 */
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(vendor bean, RedirectAttributes redirectAttributes) {
+	public String update(vendor bean, RedirectAttributes redirectAttributes, @RequestParam("imageFile") MultipartFile file, HttpSession session) {
 		if (!isValid(bean)) {
 			return ERROR_VIEW;
+		}
+		if(file.getSize()!=0){
+			String relativePath = "upload\\image\\vendingMachine\\" + date2FileName("yyyyMMdd_HHmmss", ".jpg");
+			String path=session.getServletContext().getRealPath("/") + relativePath;
+			File newFile=new File(path);
+			//通过CommonsMultipartFile的方法直接写文件
+	        try {
+				file.transferTo(newFile);
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+				return ERROR_VIEW;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return ERROR_VIEW;
+			}
+	        bean.setImage(relativePath);
 		}
 		vendingMachineService.update(bean, "orders");
 		addFlashMessage(redirectAttributes, SUCCESS_MESSAGE);
@@ -87,5 +125,21 @@ public class VendingMachineController extends BaseController {
 	public @ResponseBody Message delete(Long[] ids) {
 		vendingMachineService.delete(ids);
 		return SUCCESS_MESSAGE;
+	}
+	
+	/**
+	 * 使用当前的系统时间来命名文件。
+	 * 
+	 * @param nameFormat
+	 *            要命名的文件的日期格式 如："yyyyMMdd_HHmmss"
+	 * @param fileType
+	 *            文件的类型 如：".mp3"
+	 * @return
+	 */
+	public static String date2FileName(String nameFormat, String fileType) {
+		Date date = new Date(System.currentTimeMillis());
+		SimpleDateFormat dateFormat = new SimpleDateFormat(nameFormat);
+		String fileName = dateFormat.format(date) + fileType;
+		return fileName;
 	}
 }
