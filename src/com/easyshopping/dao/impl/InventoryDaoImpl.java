31d0ea638a88614bcf7df71d85d9b0955916a9cc
Administrator <Administrator@2012-20130905XL>
2017-03-04 11:41:45 +0800

    库存查询接口

--- src/com/easyshopping/dao/impl/InventoryDaoImpl.java
+++ src/com/easyshopping/dao/impl/InventoryDaoImpl.java
@@ -7,7 +7,9 @@ package com.easyshopping.dao.impl;
 
 import java.util.List;
 
+import javax.management.Query;
 import javax.persistence.FlushModeType;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -25,8 +27,17 @@ public class InventoryDaoImpl extends BaseDaoImpl<Inventory, Long> implements In
 
 	@Override
 	public List<Inventory> queryCount(long vendor_id, long product_id) {
-		String jpql = "select inventorys from Inventory inventorys where inventorys.vendor_id = :vendor_id and inventorys.product_id = :product_id order by inventorys.createDate desc";
-		return entityManager.createQuery(jpql, Inventory.class).setFlushMode(FlushModeType.COMMIT).setParameter("vendor_id", vendor_id).setParameter("product_id", product_id).getResultList();
+		String jpql = "select inventorys from Inventory inventorys where inventorys.vendor_id = :vendor_id ";
+		if(product_id!=-1){
+			jpql = jpql +" and inventorys.product_id = :product_id ";
+		}
+		jpql = jpql + " order by inventorys.createDate desc";
+		TypedQuery<Inventory> query = entityManager.createQuery(jpql, Inventory.class);
+		query.setFlushMode(FlushModeType.COMMIT).setParameter("vendor_id", vendor_id);
+		if(product_id!=-1){
+			query.setParameter("product_id", product_id);
+		}
+		return query.getResultList();
 	}
 
 }
